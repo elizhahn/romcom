99dde6d5d55d2213fc99062da6ebf78f2b7ab36e
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-13 11:27:47 -0700

    Refactor: create Display Cover function, prevent default reload on form submission

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Book Cover HTML elements
 var coverImage = document.querySelector(".cover-image");
 var title = document.querySelector(".cover-title");
-var tagLine = document.querySelector(".tagline");
+var tagline = document.querySelector(".tagline");
 var descriptor1 = document.querySelector(".tagline-1");
 var descriptor2 = document.querySelector(".tagline-2");
 //Control buttons HTML elements
@@ -20,12 +20,12 @@ var inputCover = document.querySelector(".user-cover")
 var inputTitle = document.querySelector(".user-title");
 var inputDescriptor1 = document.querySelector(".user-desc1");
 var inputDescriptor2 = document.querySelector(".user-desc2");
-var form = document.querySelector("form");
 //Saved book covers/HTML elements
 var viewSavedCovers = document.querySelector(".saved-covers-section");
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+
 
 // Event listeners
 window.addEventListener("load", createRandomCover);
@@ -34,13 +34,8 @@ btnRandomCover.addEventListener("click", createRandomCover);
 btnViewSavedCover.addEventListener("click", showSaved);
 btnHome.addEventListener("click", returnHome);
 btnMakeMyBook.addEventListener("click", createNewCover);
-form.addEventListener('submit', handleForm);
 btnSaveCover.addEventListener('click', saveCover);
 
-//Prevents page reload upon submitting form
-function handleForm(event) {
-  event.preventDefault();
-};
 
 //Hides home view and associated buttons, displays home button
 function leaveHome() {
@@ -73,11 +68,16 @@ function createRandomCover() {
   var descrip1Instance = descriptors[getRandomIndex(descriptors)];
   var descrip2Instance = descriptors[getRandomIndex(descriptors)];
   var randomCover = new Cover(coverInstance, titleInstance, descrip1Instance, descrip2Instance);
-  coverImage.src = randomCover.cover;
-  title.textContent = randomCover.title;
-  descriptor1.textContent = randomCover.tagline1;
-  descriptor2.textContent = randomCover.tagline2;
+  displayCover(randomCover);
   returnHome();
+};
+
+//Displays Cover instance on homepage
+function displayCover(cover) {
+  coverImage.src = cover.cover;
+    title.textContent = cover.title;
+    descriptor1.textContent = cover.tagline1;
+    descriptor2.textContent = cover.tagline2;
 };
 
 //Event handler for btnMakeNewCover
@@ -97,10 +97,10 @@ function showSaved() {
   viewSavedCovers.innerHTML = "";
   for (var i = 0; i < savedCovers.length; i++) {
   viewSavedCovers.innerHTML += `<section class="main-cover mini-cover">
-    <img class="cover-image" src="${savedCovers[i].cover}">
-    <h2 class="cover-title">${savedCovers[i].title}</h2>
-    <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
-    <img class="price-tag" src="./assets/price.png">
+    <img class="cover-image" src="${savedCovers[i].cover}" id="${savedCovers[i].id}">
+    <h2 class="cover-title" id="${savedCovers[i].id}">${savedCovers[i].title}</h2>
+    <h3 class="tagline" id="${savedCovers[i].id}">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+    <img class="price-tag" id="${savedCovers[i].id}" src="./assets/price.png">
     <img class="overlay" src="./assets/overlay.png">
   </section>`;
   };
@@ -108,13 +108,10 @@ function showSaved() {
 
 
 //Event handler for btnMakeMyBook
-function createNewCover() {
+function createNewCover(event) {
   // Create new Cover instance using input values from form, display on home view
   var createdCover = new Cover(inputCover.value, inputTitle.value, inputDescriptor1.value, inputDescriptor2.value);
-  coverImage.src = createdCover.cover;
-  title.textContent = createdCover.title;
-  descriptor1.textContent = createdCover.tagline1;
-  descriptor2.textContent = createdCover.tagline2;
+  displayCover(createdCover);
   returnHome();
   //Update arrays to include input values from form
   covers.push(inputCover.value);
@@ -123,6 +120,7 @@ function createNewCover() {
   descriptors.push(inputDescriptor2.value);
   //Save created Cover to hidden array
   hiddenCover = createdCover;
+  event.preventDefault();
 };
 
 //Checks user's cover for duplicates before adding to Saved Covers array
